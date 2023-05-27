3172928918e27b67514a0c14b37f3ad3aabd104a
danierazome <j.erao@uniandes.edu.co>
2023-05-26 22:57:44 -0500

    monkey addes

---
+++ reconocimiento/monkey/cypress/support/commands.js
@@ -0,0 +1,35 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add("login", (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.session("LoginSession", () => {
+    cy.visit("http://localhost:2368/ghost/#/signin");
+    cy.get("[data-test=name]").type(email);
+    cy.get("[data-test=password]").type(password);
+    cy.get("form").contains("Login").click();
+    cy.url().should("contain", "/login-successful");
+  });
+});
